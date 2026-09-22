f6ca6a60b8af76240754dcac7a09309d4720e2a8
beboshawket30 <beboshawket30@gmail.com>
2026-09-22 21:47:20 +0300

    Complete dealer review and sentiment integration

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -18,8 +18,11 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
-
+from django.views.generic import TemplateView
 urlpatterns = [
+    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('contact/',TemplateView.as_view(template_name="Contact.html")),
